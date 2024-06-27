7d794cdca7b40e8a639161884520dde27ac7625c
prolixmagus <jeremypfishman@gmail.com>
2024-06-26 19:05:15 -0700

    Link to contentful cloud CMS

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
+import contentful from 'contentful';
+// import feet from './families.json' with { "type": "json"};
 
+
+//for CMS
+const client = contentful.createClient({
+  space: 'hjp080e02fag',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: 'tDCw5s7UkwTWtLsEpdIXR_Go8xmmBHTeQ3G7fQXARY0'
+})
+
+
+//for Railway
 dotenv.config();
+
+
+//call json data
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 let familyData = '';
 
 fs.readFile('./families.json', 'utf8', (err, data) => {
@@ -13,44 +29,79 @@ fs.readFile('./families.json', 'utf8', (err, data) => {
     return;
   }
     familyData = JSON.parse(data);
-    familyData.sort((a, b) => a.family.localeCompare(b.family));
+    familyData.sort((a, b) => a.name.localeCompare(b.name));
 });
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+//set up express
 const app = express();
 const port = process.env.PORT || 1982;
+
+
+//set up ejs
+app.set('view engine', 'ejs');
+
 
 // auto serve up static files in the public directory
 app.use(express.static('public'));
 app.use(express.static('css'));
 
-// Set up ejs view engine
-
-app.set('view engine', 'ejs');
 
 // Page routing
 app.get('/', function(request, response) {
-	//can send in a variable or list or complex object
-	response.render('home', { 
-        pageName: 'The Family Tree', 
-        intro: 'default', 
-    });
+	client.getEntry('1TP4y5ICz7rd5HwZnn5eMX')
+  .then((entry) => {
+    response.render('home', { page: entry.fields });
+  })
+  .catch(console.error)
 })
 
 app.get('/language-list', function(request, response) {
-    //can send in a variable or list or complex object
-    response.render('language-list', { 
-        pageName: 'default', 
-        intro: 'default', 
-        familyData: familyData
+  client.getEntries({
+    content_type: 'family'
+  })
+  .then(function (data) {
+    const familyData = data.items.map ((item) => {
+      return {
+        id: item.sys.id,
+        name: item.fields.name,
+        mostSpoken: item.fields.mostSpoken,
+        population: item.fields.population,
+        languages: item.fields.languages,
+        image: item.fields.image.fields.file.url,
+        synthetic: item.fields.synthetic,
+        countries: item.fields.countries,
+        location: item.fields.location
+      }
     });
+    response.render('language-list', {family : familyData});
+  });
 })
 
-app.get('/detail/:slug', function(request, response) {
-    const family = familyData.find((family) => family.slug === request.params.slug);
-    response.render('detail', { family })
-})
+app.get('/detail/:id', function(request, response) {
+  client.getEntries({
+    content_type: 'family'
+  })
+  .then(function (data) {
+    const familyData = data.items.map ((item) => {
+      return {
+        id: item.sys.id,
+        name: item.fields.name,
+        mostSpoken: item.fields.mostSpoken,
+        population: item.fields.population,
+        languages: item.fields.languages,
+        image: item.fields.image.fields.file.url,
+        description: item.fields.image.fields.description,
+        alt: item.fields.image.fields.title,
+        synthetic: item.fields.synthetic,
+        countries: item.fields.countries,
+        location: item.fields.location
+      }
+    });
+    const family = familyData.find((family) => family.id === request.params.id);
+    response.render('detail', { family });
+  })
+});;
 
 // If no route defined ... 404 error
 app.use(function(request, response) {
@@ -61,6 +112,4 @@ app.listen(port, function(error) {
 	if (error) console.log(error)
 	console.log(`Server listening on PORT: ${port}`)
 });
-
-
 
